0953ef7cfd83b92872b0abd40c9029150ca3755b
rijiu915 <2088663893jj@gmail.com>
2026-01-26 17:46:03 +0800

    Auto-sync after agent response

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -3,6 +3,7 @@
 import { useSession } from 'next-auth/react';
 import { useEffect, useState } from 'react';
 import { useRouter } from 'next/navigation';
+import Link from 'next/link';
 import { Navbar } from '@/components/navbar';
 
 interface PointsRecord {
