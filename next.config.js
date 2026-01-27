e58af7802abfa980ecb7c84592ae54d11e80cb22
rijiu915 <2088663893jj@gmail.com>
2026-01-27 09:47:46 +0800

    Auto-sync after agent response

--- next.config.js
+++ next.config.js
@@ -1,6 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'pub-15ea01688b984f9f900edca1c6917f4c.r2.dev',
+      },
+      {
+        protocol: 'https',
+        hostname: 'slelguoygbfzlpylpxfs.supabase.co',
+      },
+    ],
+  },
 };
 
 module.exports = nextConfig;
